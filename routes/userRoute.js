ca0a4d2ffa724ff49ff9d9bfef54e41bf905dc45
asprindb <asprindb@arch.localdomain>
2022-05-12 14:28:54 +0430

    Add Some Routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const contactController = require("./../controllers/contactController");
+
+router.route("/").get(contactController.getAllUsers);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-
 const bodyparser = require("body-parser");
+//Import ContactController
 const contactController = require("./controllers/contactController");
+//Import Route
+const userRoute = require("./routes/userRoute");
+
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
+//Route MiddleWares
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT, () => {
     console.log("The Server is up And Running on port 3000");

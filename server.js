a1c31613d818cb6c6671a423d0dcf7d176e55e55
asprindb <asprindb@arch.localdomain>
2022-05-12 17:04:57 +0430

    Connect to DATABASE

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const bodyparser = require("body-parser");
 //Import ContactController
 const contactController = require("./controllers/contactController");
 //Import Route
 const userRoute = require("./routes/contactsRoute");
 //Import DATABASE
+const mongoose = require("mongoose");
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
+//Connect to DATABASE:
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log("Connected To DB");
+    },
+);
 //Route MiddleWares
 app.use("/api/contacts", contactsRoute);
 

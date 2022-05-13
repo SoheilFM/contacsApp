7591ba1db223b1f5fba25893b9e6965e9a23db07
asprindb <asprindb@arch.localdomain>
2022-05-13 23:21:34 +0430

    Install JOI

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -5,24 +5,28 @@ const contactsModel = require("../model/contactsModel");
 const contactsController = "./../controllers/contactsController";
 
 //.get(contactsController.getAllContacts)
-router.route("/create").post(contactsController.createContact);
+//router.route("/create").post(contactsController.createContact);
 // router
 //   .route("/:id")
 //   .get(contactsController.getContact)
 //   .patch(contactsController.updateContact)
 //   .delete(contactsController.deteleContact);
 
-// router.post("/create", async (req, res) => {
-//   const contacts = new contactsModel({
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     mobile: req.body.mobile,
-//     email: req.body.email,
-//   });
-//   const savedContacts = await contacts.save();
-//   res.send(savedContacts);
-//   console.log(req.body);
-//   console.log(savedContacts);
-// });
+router.post("/create", async (req, res) => {
+  try {
+    const contacts = new contactsModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      mobile: req.body.mobile,
+      email: req.body.email,
+    });
+    const savedContacts = await contacts.save();
+    res.send(savedContacts);
+    console.log(req.body);
+    console.log(savedContacts);
+  } catch (err) {
+    res.status(400);
+  }
+});
 
 module.exports = router;
